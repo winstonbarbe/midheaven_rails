b8cdf0966daba3ffcf80cfaf2bc0a7f2bd8a9d70
winstonbarbe <winston.barbe@gmail.com>
2021-01-03 22:14:35 -0500

    creates users show, controller, view

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,4 +17,9 @@ class Api::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    render "show.json.jb"
+  end
 end
